23ece604f8bcfb203e0cca0db572dd831f7ed508
Josh Cole <saikojosh@gmail.com>
2017-05-10 15:59:11 +0100

    Fix - whitespace on tests converted to tabs

--- tests/post.js
+++ tests/post.js
@@ -17,40 +17,40 @@ const RequestNinja = require('../index');
 
 describe('Request: POST', () => {
 
-  it('should return data using a promise when a URI is requested using URL mode', function (done) {
-    this.timeout(1000 * 5);
+	it('should return data using a promise when a URI is requested using URL mode', function (done) {
+		this.timeout(1000 * 5);
 
-    const req = new RequestNinja('http://httpbin.org/post');
+		const req = new RequestNinja('http://httpbin.org/post');
 
-    req.go({ var1: 123, var2: 'abc' })
-      .then((result) => JSON.parse(result))
-      .then((result) => {
-        expect(result).to.be.ok;
-        const data = JSON.parse(result.data);
-        expect(data).to.have.a.property('var1', 123);
-        expect(data).to.have.a.property('var2', 'abc');
-        return result;
-      })
-      .then((result) => done())
-      .catch((err) => done(err));
+		req.go({ var1: 123, var2: 'abc' })
+			.then((result) => JSON.parse(result))
+			.then((result) => {
+				expect(result).to.be.ok;
+				const data = JSON.parse(result.data);
+				expect(data).to.have.a.property('var1', 123);
+				expect(data).to.have.a.property('var2', 'abc');
+				return result;
+			})
+			.then((result) => done())
+			.catch((err) => done(err));
 
-  });
+	});
 
-  it('should return data using a callback when a URI is requested using URL mode', function (done) {
-    this.timeout(1000 * 5);
+	it('should return data using a callback when a URI is requested using URL mode', function (done) {
+		this.timeout(1000 * 5);
 
-    const req = new RequestNinja('http://httpbin.org/post');
+		const req = new RequestNinja('http://httpbin.org/post');
 
-    req.go({ var1: 123, var2: 'abc' }, (err, _result) => {
-      if (err) { return done(err); }
-      expect(_result).to.be.ok;
-      const result = JSON.parse(_result);
-      const data = JSON.parse(result.data);
-      expect(data).to.have.a.property('var1', 123);
-      expect(data).to.have.a.property('var2', 'abc');
-      return done();
-    });
+		req.go({ var1: 123, var2: 'abc' }, (err, _result) => {
+			if (err) { return done(err); }
+			expect(_result).to.be.ok;
+			const result = JSON.parse(_result);
+			const data = JSON.parse(result.data);
+			expect(data).to.have.a.property('var1', 123);
+			expect(data).to.have.a.property('var2', 'abc');
+			return done();
+		});
 
-  });
+	});
 
 });
